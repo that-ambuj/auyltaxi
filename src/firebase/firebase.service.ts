38672eaf4a4a5a1ffddc3c4d65552a271ddc2b5c
Ambuj Singh <97109381+that-ambuj@users.noreply.github.com>
2023-10-23 00:32:48 +0530

    feat(notification): add FirebaseService to send notifications

---
+++ src/firebase/firebase.service.ts
@@ -0,0 +1,37 @@
+import { ProfileService } from "@app/profile/profile.service";
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { initializeApp, applicationDefault } from "firebase-admin/app";
+import { getMessaging } from "firebase-admin/messaging";
+
+@Injectable()
+export class FirebaseService implements OnModuleInit {
+  constructor(private readonly profileService: ProfileService) {}
+
+  async onModuleInit() {
+    initializeApp({
+      credential: applicationDefault(),
+    });
+    console.info("Successfully initiated FirebaseService.");
+  }
+
+  async sendNotification({
+    user_id,
+    message,
+  }: {
+    user_id: string;
+    message: string;
+  }) {
+    try {
+      const user = await this.profileService.findById(user_id);
+
+      const token = user.device_token;
+
+      const res = await getMessaging().send({ data: { message }, token });
+      console.info("Successfully sent notification from firebase:", res);
+
+      return;
+    } catch (e) {
+      console.error("Error sending message/notification from firebase:", e);
+    }
+  }
+}
